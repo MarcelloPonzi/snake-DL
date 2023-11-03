d3b20028244d37a8ae32f4c838db2c5c0ae09b0d
MarcelloPonzi <91527254+MarcelloPonzi@users.noreply.github.com>
2023-11-03 18:07:20 +0100

    corretto device utilizzato

--- DQN.py
+++ DQN.py
@@ -12,7 +12,7 @@ from game import Game
 from player import Player
 
 # from snakeClass import Food, Game, Player
-DEVICE = 'cpu' # 'cuda' if torch.cuda.is_available() else 'cpu'
+DEVICE = 'cuda' # 'cuda' if torch.cuda.is_available() else 'cpu'
 
 class DQNAgent(torch.nn.Module):
     def __init__(self, params):
